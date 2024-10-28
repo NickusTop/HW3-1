9ef5251f797a4c89a6a244430531f23344bb1d8c
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-10-28 18:27:24 +0200

    Homework

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let age = "13";
+let name = "Mykyta";
+let isStudent = false;
+let myString = "Допобачення!";
+let myNumber = 20;
+myNumber += 10
+let myNull = null;
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(myString);
+console.log(myNumber);
+console.log(myNull);
