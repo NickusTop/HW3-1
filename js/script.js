4a724dd9d32fad207b9730b5f5c6c4bc3a517324
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-11-04 18:30:43 +0200

    f

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ console.log(isStudent);
 console.log(myString);
 console.log(myNumber);
 console.log(myNull);
+let userName = prompt("Введіть ваше ім'я:");
+alert("Привіт " + userName + "!");
+let confrm = confirm("Ви підтверджуєте дію?");
+if (confrm) {
+    alert("Дякую за підтвердження!");
+}
+    else {
+     alert("Дію відмінено!");
+    }
+alert("Увага! Ця дія небезпечна.");
+let danger = confirm("Ви впевнені, що хочете продовжити?");
+if (danger) {
+    alert("Дякую за підтвердження!");
+}
+    else {
+     alert("Дію відмінено!");
+    }
